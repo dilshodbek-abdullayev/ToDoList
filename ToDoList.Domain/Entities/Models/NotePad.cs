f708679b80105c5c30a959edd380bde02ca6eada
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-03-03 14:50:16 +0500

    Add project files.

---
+++ ToDoList.Domain/Entities/Models/NotePad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Domain.Entities.Models
+{
+    public class NotePad
+    {
+        public int Id { get; set; }
+        public string Note { get; set; }
+        public DateTime To =DateTime.Now.ToLocalTime();
+        public DateTime Do { get; set; }
+
+    }
+}
---
+++ ToDoList.Domain/Entities/ViewModels/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Domain.Entities.ViewModels
+{
+    public class UserViewModel
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string Role { get; set; }
+    }
+}
